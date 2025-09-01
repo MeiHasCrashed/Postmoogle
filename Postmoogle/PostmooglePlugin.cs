7d41f942b7dd915ba3025d44ee651ddc3fc1eeb9
MeiHasCrashed <meihascrashed@outlook.com>
2025-09-01 18:18:48 +0200

    Initial repository setup

---
+++ Postmoogle/PostmooglePlugin.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2025 MeiHasCrashed
+// License: AGPL-3.0-or-later (see /License.md)
+
+using Dalamud.Plugin;
+
+namespace Postmoogle;
+
+public class PostmooglePlugin : IDalamudPlugin
+{
+
+    public PostmooglePlugin()
+    {
+        
+    }
+
+    private void SetupContainer()
+    {
+        
+    }
+    
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+﻿# Postmoogle Plugin
+A Dalamud plugin to send you notifications on discord/ntfy based on game events.
